6a7b08028ba23bc8845e395c0cdffd0e96e92def
yitaxede <yitaxede@mail-share.com>
2019-04-07 00:11:38 -1100

    Added kdf time comparison table for GOST pbkdf2

--- crypto/pygost_example.py
+++ crypto/pygost_example.py
@@ -3,16 +3,40 @@ from pygost.gost3412 import GOST3412Kuznechik
 from pygost.gost34112012512 import pbkdf2
 from pygost.gost3413 import cfb_decrypt
 from pygost.gost3413 import cfb_encrypt
+from time import time
 
 # Kuznyechick cipher with CFB mode
+'''
+KDF time depending on iterations
 
-iterations = 10
+Iters   Time
+1 		0.045s
+2 		0.096s
+4 		0.198s
+8 		0.399s
+16 		0.767s
+32 		1.530s
+64 		3.065s
+128 	6.117s
+256 	12.255s
+512 	24.900s
+1024 	48.612s
+'''
+
+iterations = 32
 keysize = 32
 key = pbkdf2(b"password", b"salt", iterations, keysize)
 ciph = GOST3412Kuznechik(key)
 
-#An initialization vector has different security requirements than a key, so the IV usually does not need to be secret. However, in most cases, it is important that an initialization vector is never reused under the same key. For CBC and CFB, reusing an IV leaks some information about the first block of plaintext, and about any common prefix shared by the two messages. For OFB and CTR, reusing an IV completely destroys security.
 
+# An initialization vector has different security requirements than a key, 
+# so the IV usually does not need to be secret. However, in most cases, 
+# it is important that an initialization vector is never reused 
+# under the same key. 
+# For CBC and CFB, reusing an IV leaks some information about 
+# the first block of plaintext, and about any common prefix shared by 
+# the two messages. 
+# For OFB and CTR, reusing an IV completely destroys security.
 iv = urandom(16 * 2)
 
 plaintext = bytearray("this is a plaintext", "utf-8")
--- crypto/pynacl_example.py
+++ crypto/pynacl_example.py
@@ -19,24 +19,24 @@ password = b"password shared between Alice and Bob"
 message = b"This is a message for Bob's eyes only"
 
 # argon2id?
-# kdf = pwhash.argon2i.kdf
-# salt = utils.random(pwhash.argon2i.SALTBYTES)
-# ops = pwhash.argon2i.OPSLIMIT_INTERACTIVE
-# mem = pwhash.argon2i.MEMLIMIT_INTERACTIVE
+kdf = pwhash.argon2i.kdf
+salt = utils.random(pwhash.argon2i.SALTBYTES)
+ops = pwhash.argon2i.OPSLIMIT_INTERACTIVE
+mem = pwhash.argon2i.MEMLIMIT_INTERACTIVE
 
 # or, if there is a need to use scrypt:
-kdf = pwhash.scrypt.kdf
-salt = utils.random(pwhash.scrypt.SALTBYTES)
-ops = pwhash.scrypt.OPSLIMIT_SENSITIVE
-mem = pwhash.scrypt.MEMLIMIT_SENSITIVE
+# kdf = pwhash.scrypt.kdf
+# salt = utils.random(pwhash.scrypt.SALTBYTES)
+# ops = pwhash.scrypt.OPSLIMIT_SENSITIVE
+# mem = pwhash.scrypt.MEMLIMIT_SENSITIVE
 
 key = kdf(secret.SecretBox.KEY_SIZE, password, salt,
                  opslimit=ops, memlimit=mem)
-'''
+
 box = secret.SecretBox(key)
 
 encrypted = box.encrypt(message)
 
 received = box.decrypt(encrypted)
 print(received.decode('utf-8'))
-'''
+
